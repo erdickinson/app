8de6e0cc543bafdcf8d727d059ed79c461e4d6c7
erdickinson <49318461+erdickinson@users.noreply.github.com>
2023-03-03 14:21:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
+import serial
 
 app = Flask(__name__)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.reset_input_buffer()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        number = request.form['number']
+        ser.write(number.encode('utf-8'))
+        arduino_response = ser.read().decode('utf-8')
+        print("Received number from Arduino:", arduino_response)
     return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def submit():
-    num = request.form['number']
-    # Send the number to the Arduino
-    return render_template('index.html', message='Number sent to Arduino!')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
