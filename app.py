5de635384ea6df2e09f8f7471eb6767588ce317d
erdickinson <49318461+erdickinson@users.noreply.github.com>
2023-03-03 17:00:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,43 @@ import serial
 app = Flask(__name__)
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
+value = 0
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", value=value)
 
-@app.route("/<value>")
-def set_value(value):
-    ser.write(value.encode())
-    return "Value sent: " + value
+@app.route("/decrease_20")
+def decrease_20():
+    global value
+    if value - 20 >= 0:
+        value -= 20
+    ser.write(str(value).encode())
+    return "Value sent: " + str(value)
+
+@app.route("/decrease_10")
+def decrease_10():
+    global value
+    if value - 10 >= 0:
+        value -= 10
+    ser.write(str(value).encode())
+    return "Value sent: " + str(value)
+
+@app.route("/increase_10")
+def increase_10():
+    global value
+    if value + 10 <= 100:
+        value += 10
+    ser.write(str(value).encode())
+    return "Value sent: " + str(value)
+
+@app.route("/increase_20")
+def increase_20():
+    global value
+    if value + 20 <= 100:
+        value += 20
+    ser.write(str(value).encode())
+    return "Value sent: " + str(value)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
