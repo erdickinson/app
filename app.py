901261499dd4b24e6b22f57b97c12975ce7fbcd4
erdickinson <49318461+erdickinson@users.noreply.github.com>
2023-03-03 17:10:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,32 +16,33 @@ def decrease_20():
     global value
     if value - 20 >= 0:
         value -= 20
-    ser.write(str(value).encode())
-    return "Value sent: " + str(value)
+    ser.write(str(value).encode() + b'\n')
+    return str(value)
 
 @app.route("/decrease_10")
 def decrease_10():
     global value
     if value - 10 >= 0:
         value -= 10
-    ser.write(str(value).encode())
-    return "Value sent: " + str(value)
+    ser.write(str(value).encode() + b'\n')
+    return str(value)
 
 @app.route("/increase_10")
 def increase_10():
     global value
     if value + 10 <= 100:
         value += 10
-    ser.write(str(value).encode())
-    return "Value sent: " + str(value)
+    ser.write(str(value).encode() + b'\n')
+    return str(value)
 
 @app.route("/increase_20")
 def increase_20():
     global value
     if value + 20 <= 100:
         value += 20
-    ser.write(str(value).encode())
-    return "Value sent: " + str(value)
+    ser.write(str(value).encode() + b'\n')
+    return str(value)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
